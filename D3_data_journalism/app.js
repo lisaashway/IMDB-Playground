4b220bd813b9dc5307284167d5f6df88c690340c
lisaashway <lisaashway@gmail.com>
2020-07-01 13:52:00 -0400

    fixed css and js files

--- D3_data_journalism/app.js
+++ D3_data_journalism/app.js
@@ -12,7 +12,7 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select(".scatter")
+var svg = d3.select(".chart")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
@@ -21,24 +21,23 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data
-d3.csv("assets/data/data.csv").then(function(movieData) {
+d3.csv("data.csv").then(function(movieData) {
 
     // Step 1: Parse Data/Cast as numbers
     // ==============================
     movieData.forEach(function(data) {
-      console.log(data.revenue);
       data.revenue = +data.revenue
-      data.rating = +data.rating;
+      data.metascore = +data.metascore;
     });
 
     // Step 2: Create scale functions
     // ==============================
     var xLinearScale = d3.scaleLinear()
-      .domain([20, d3.max(movieData, d => d.revenue)])
+      .domain([0,d3.max(movieData, d => d.metascore)])
       .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(movieData, d => d.rating)])
+      .domain(d3.extent(movieData, d => d.revenue))
       .range([height, 0]);
 
     // Step 3: Create axis functions
@@ -57,38 +56,42 @@ d3.csv("assets/data/data.csv").then(function(movieData) {
 
     // Step 5: Create Circles
     // ==============================
+
+    var myColor = d3.scaleLinear().domain(d3.extent(movieData, d=> d.revenue))
+    .range(["#44D1C8", "#FF5CDA"])
+
     var circlesGroup = chartGroup.selectAll("circle")
     .data(movieData)
     .enter()
     .append("circle")
-    .attr("cx", d => xLinearScale(d.revenue))
-    .attr("cy", d => yLinearScale(d.rating))
-    .attr("r", "15")
-    .attr("fill", "pink")
-    .attr("opacity", ".5");
+    .attr("cx", d => xLinearScale(d.metascore))
+    .attr("cy", d => yLinearScale(d.revenue))
+    .attr("r", "5")
+    .attr("fill", d => myColor(d.revenue))
+    .attr("opacity", ".9");
 
-    // // Step 6: Initialize tool tip
-    // // ==============================
-    // var toolTip = d3.tip()
-    //   .attr("class", "tooltip")
-    //   .offset([80, -60])
-    //   .html(function(d) {
-    //     return (`${d.Title}<br>Actors: ${d.Actors}<br>Year: ${d.Year}`);
-    //   });
+    // Step 6: Initialize tool tip
+    // ==============================
+    var toolTip = d3.tip()
+      .attr("class", "tooltip")
+      .offset([80, -60])
+      .html(function(d) {
+        return (d.title + "<br>Year: " + d.year + "<br>Revenue: $" + d.revenue + " Million");
+      });
 
-    // // Step 7: Create tooltip in the chart
-    // // ==============================
-    // chartGroup.call(toolTip);
+    // Step 7: Create tooltip in the chart
+    // ==============================
+    chartGroup.call(toolTip);
 
-    // // Step 8: Create event listeners to display and hide the tooltip
-    // // ==============================
-    // circlesGroup.on("click", function(data) {
-    //   toolTip.show(data, this);
-    // })
-    //   // onmouseout event
-    //   .on("mouseout", function(data, index) {
-    //     toolTip.hide(data);
-    //   });
+    // Step 8: Create event listeners to display and hide the tooltip
+    // ==============================
+    circlesGroup.on("click", function(data) {
+      toolTip.show(data, this);
+    })
+      // onmouseout event
+      .on("mouseout", function(data, index) {
+        toolTip.hide(data);
+      });
 
     // Create axes labels
     chartGroup.append("text")
@@ -97,10 +100,10 @@ d3.csv("assets/data/data.csv").then(function(movieData) {
       .attr("x", 0 - (height / 2))
       .attr("dy", "1em")
       .attr("class", "axisText")
-      .text("Number of Billboard 100 Hits");
+      .text("Revenue (in Millions)");
 
     chartGroup.append("text")
       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
       .attr("class", "axisText")
-      .text("Hair Metal Band Hair Length (inches)");
+      .text("Metascore");
   })
